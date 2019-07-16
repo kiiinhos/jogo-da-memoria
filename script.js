f3d113bea595492747934bafc400b31d9026c0cb
kiiinhos <viniciuscamargo713@gmail.com>
2019-07-16 17:22:26 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
-
-
-
 let quadradinhos = document.querySelectorAll("div")
 
 function MudarCor(event){
-    event.target.style.backgroundColor = "yellow";
+     event.target.style.backgroundColor = ("")
+    event.target.classList.toggle("");
 }
-for( let quadradinho of quadradinhos){
+
+for(let quadradinho of quadradinhos){
     quadradinho.onclick = MudarCor;
 }
+
+if(event.classList.contains()){
+    classList.remove("");
+}
+else{
+    classList.add("");
+}
+
+
+// Criar lista para as cores
+// usar os veteros
